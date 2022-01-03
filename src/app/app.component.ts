82788e17343c69304561a0ab66582ab6d5f1c4e1
Nattaphon Phummoung <bas.zealot13@gmail.com>
2022-01-03 22:52:08 +0700

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'backend';
+export class AppComponent implements OnInit {
+    toggle: Element;
+    nav: Element;
+    bodypd: Element;
+    headerpd: Element;
+    // linkColor: any;
+
+    ngOnInit() {
+        this.toggle = document.getElementById('header-toggle');
+        this.nav = document.getElementById('nav-bar');
+        this.bodypd = document.getElementById('body-pd');
+        this.headerpd = document.getElementById('header');
+        // this.linkColor = document.querySelectorAll('.nav_link');
+    }
+
+    toggleClick() {
+        this.nav.classList.toggle('show');
+        this.toggle.classList.toggle('bx-x');
+        this.bodypd.classList.toggle('body-pd');
+        this.headerpd.classList.toggle('body-pd');
+    }
+
+    // colorLink() {
+    //     if (this.linkColor) {
+    //         this.linkColor.forEach(l => l.classList.remove('active'))
+    //         this.classList.add('active')
+    //     }
+    // }
 }
